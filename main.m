034795a932225c220165ea8f1de93b20a6fc3594
Aridan <aridanpantoja@gmail.com>
2024-10-10 09:58:09 -0300

    first commit

---
+++ main.m
@@ -0,0 +1,24 @@
+% Trabalho feito por: Aridan Pantoja, Karol Wojtyla e Manuela Nunes
+
+close all;  clc;
+
+% Parâmetros Gerais 
+
+M = 100;                                                % Número de antenas
+K = 40;                                                 % Número de usuários
+area = 1000 * 1000;                                     % Área de simulação (1km²)
+pot_down = 100e-3;                                      % Potência de Downlink (100mW)
+pot_up = 100e-3;                                        % Potência de Upplink (100mW)
+BW = 20e6;                                              % Largura de Banda (20MHz)
+Tc = 196;                                               % Tempo de Coerência
+Tp = min(K, Tc);                                        % Tempo para estimativa do canal
+noise = 9;                                              % Ruído (9dB)
+
+% Canal Real
+
+betas = 1 ./ (distances_ue_ap.^3.8);
+
+% Potência Normalizada
+
+pot_noise = 20e6 * 1.381e-23 * 290* 10^(noise/10);      % Potência do Ruído
+pot_norm = pot_up / pot_noise;                          % Potência Normalizada
